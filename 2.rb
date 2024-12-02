ca0e64ed099092fccfb47b8cc057f80a51280d50
Javier Arevalo <jare@iguanademos.com>
2024-12-02 15:02:37 +0100

    slight polish of day 2

--- 2.rb
+++ 2.rb
@@ -1,9 +1,9 @@
 nums = ARGF.each.map { | l | l.split.map(&:to_i) }
 
-def valid_report(r)
-  d = r.each_cons(2).map { | (a, b) | (a-b) }
-  d.all? { _1.between?(1,3) } || d.all? { _1.between?(-3,-1)}
+def valid_report?(r)
+  deltas = r.each_cons(2).map { | (a, b) | (a-b) }
+  deltas.all? { _1.between?(1,3) } || deltas.all? { _1.between?(-3,-1) }
 end
 
-puts nums.filter { | l | valid_report(l) } .count
-puts nums.filter { | l | (0...l.length).any? { | i | valid_report(l[0...i] + l[(i+1)..]) } } .count
+puts nums.filter { | r | valid_report?(r) } .count
+puts nums.filter { | r | (0...r.length).any? { | i | valid_report?(r[0...i] + r[(i+1)..]) } } .count
