163ecf1c6070bd467b325d4254c771a7936ff1a4
Javier Arevalo <jare@iguanademos.com>
2024-12-04 15:51:14 +0100

    day 4

---
+++ 4.rb
@@ -0,0 +1,25 @@
+$input = ARGF.each.to_a
+$w = $input[0].length
+$h = $input.length
+
+def at(x, y)
+  return (x < 0 || x >= $w || y < 0 || y >= $h)? "." : $input[y][x]
+end
+
+def word?(x, y, dx, dy, *words)
+  words.any? { _1.chars.each_with_index.all? { | c, i | at(x + dx*i, y + dy*i) == c } }
+end
+
+directions = [[-1,0],[-1,-1],[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1]]
+
+p Enumerator::product(0...$w, 0...$h, directions).count { |x, y, d| 
+  word?(x, y, d[0], d[1], "XMAS")
+}
+
+def xword?(x, y, word)
+  word?(x, y, 1, 1, word, word.reverse) && word?(x+word.length-1, y, -1, 1, word, word.reverse)
+end
+
+p Enumerator::product(0...$w, 0...$h).count { |x, y| 
+  xword?(x, y, "MAS")
+}
