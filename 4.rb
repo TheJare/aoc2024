a30a1a285587ee73d2201ec5d29010918f2352fe
Javier Arevalo <jare@iguanademos.com>
2024-12-04 19:11:27 +0100

    day 4 minor cleanup

--- 4.rb
+++ 4.rb
@@ -3,21 +3,23 @@ $w = $input[0].length
 $h = $input.length
 
 def at(x, y)
-  return (x < 0 || x >= $w || y < 0 || y >= $h)? "." : $input[y][x]
+  (x < 0 || x >= $w || y < 0 || y >= $h)? "." : $input[y][x]
 end
 
-def word?(x, y, dx, dy, *words)
-  words.any? { _1.chars.each_with_index.all? { | c, i | at(x + dx*i, y + dy*i) == c } }
+def word?(x, y, dx, dy, word)
+  [word, word.reverse].any? {
+    _1.chars.each_with_index.all? { | c, i | at(x + dx*i, y + dy*i) == c }
+  }
 end
 
-directions = [[-1,0],[-1,-1],[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1]]
+directions = [[1,0],[1,1],[0,1],[-1,1]]
 
 p Enumerator::product(0...$w, 0...$h, directions).count { |x, y, d| 
   word?(x, y, d[0], d[1], "XMAS")
 }
 
 def xword?(x, y, word)
-  word?(x, y, 1, 1, word, word.reverse) && word?(x+word.length-1, y, -1, 1, word, word.reverse)
+  word?(x, y, 1, 1, word) && word?(x+word.length-1, y, -1, 1, word)
 end
 
 p Enumerator::product(0...$w, 0...$h).count { |x, y| 
