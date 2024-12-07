6146c8b962866b5bbc0757cd1b8b25d18661edbf
Javier Arevalo <jare@iguanademos.com>
2024-12-07 14:59:58 +0100

    slight optimization for day 7

--- 7.rb
+++ 7.rb
@@ -3,8 +3,11 @@ input = ARGF.map { | l | l.strip.split(/:? /).map(&:to_i)}.to_a
 # brute force galore
 def sum_valid_equations(input, operators)
     input.filter { | eq |
-    operators.repeated_permutation(eq.length - 2).any? do | p |
-      eq[0] == p.zip(eq[2..]).reduce(eq[1]) {| a, (op, b) | op.call(a, b) }
+    operators.repeated_permutation(eq.length - 2).any? -> (p) do 
+      eq[0] == p.zip(eq[2..]).reduce(eq[1]) do | a, (op, b) |
+        return false if a > eq[0]
+        op.call(a, b)
+      end
     end
   }. sum { | eq | eq[0]}
 end
